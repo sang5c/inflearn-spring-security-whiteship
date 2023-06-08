416e16e7e03a9ca21432de6a09b9428da77f9689
sang5c <skah321@gmail.com>
2023-06-08 15:40:58 +0900

    temp

--- src/main/java/com/example/inflearnspringsecuritywhiteship/config/SecurityConfig.java
+++ src/main/java/com/example/inflearnspringsecuritywhiteship/config/SecurityConfig.java
@@ -3,11 +3,7 @@ package com.example.inflearnspringsecuritywhiteship.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -15,7 +11,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -38,15 +33,15 @@ public class SecurityConfig {
     /**
      * 참고: https://stackoverflow.com/questions/74763256/accessdecisionvoter-deprecated-with-spring-security-6-x
      */
-    @Bean
-    public DefaultWebSecurityExpressionHandler expressionHandler() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-
-        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
-        handler.setRoleHierarchy(roleHierarchy);
-        return handler;
-    }
+//    @Bean
+//    public DefaultWebSecurityExpressionHandler expressionHandler() {
+//        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+//        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+//
+//        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+//        handler.setRoleHierarchy(roleHierarchy);
+//        return handler;
+//    }
 
     /**
      * 참고: https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#jc-httpsecurity
--- src/test/java/com/example/inflearnspringsecuritywhiteship/form/SampleServiceTest.java
+++ src/test/java/com/example/inflearnspringsecuritywhiteship/form/SampleServiceTest.java
@@ -11,8 +11,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
 
 @SpringBootTest
 class SampleServiceTest {
@@ -37,15 +41,39 @@ class SampleServiceTest {
 
     @Test
     void dashboard() {
-        Account account = new Account("user", "123", "USER");
-        accountService.createNew(account);
+//        Account account = new Account("asdf", "123", "ADMIN");
+//        accountService.createNew(account);
 
-        UserDetails userDetails = accountService.loadUserByUsername("user");
+        UserDetails userDetails = accountService.loadUserByUsername("pso");
+//        UserDetails userDetails = User.builder()
+//                .username("tester")
+//                .roles("TESTER")
+//                .build();
 
+//        User user = new User("tester", "", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, "123");
         Authentication authentication = authenticationManager.authenticate(token);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        sampleService.dashboard();
+    }
+    @Test
+    void dashboard2() {
+        Account account = new Account("user", "123", "USER");
+        accountService.createNew(account);
+
+//        UserDetails userDetails = accountService.loadUserByUsername("user");
+//        UserDetails userDetails = User.builder()
+//                .username("tester")
+//                .roles("TESTER")
+//                .build();
+
+        User user = new User("tester", "", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+//        Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(token);
 
         sampleService.dashboard();
     }
